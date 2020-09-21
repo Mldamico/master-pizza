2e4d6cfd44d51ce2578c3ae5c0ee6a0d9251bae6
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-21 16:24:13 -0300

    Global styles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Footer } from './Footer';
 import { Nav } from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+
 export const Layout = ({ children }) => {
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
