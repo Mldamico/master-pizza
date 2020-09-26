2d1eff6a41896153bc865676a0c5f909783ce4f2
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-26 12:18:22 -0300

    Grid and subgrid of pizza

--- gatsby/src/components/PizzaItem.js
+++ gatsby/src/components/PizzaItem.js
@@ -1,18 +1,35 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 
+const PizzaStyles = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(
+    --rows,
+    subgrid
+  ); //Take your row sizing not from this style, but from parent
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 export const PizzaItem = ({ pizza }) => {
   console.log(pizza);
   return (
-    <div>
+    <PizzaStyles>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
           <span className='mark'>{pizza.name}</span>
         </h2>
-        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
-        <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
       </Link>
-    </div>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+    </PizzaStyles>
   );
 };
--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { PizzaItem } from './PizzaItem';
+import styled from 'styled-components';
 
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
 export const PizzaList = ({ pizzas }) => {
   return (
-    <div>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <PizzaItem key={pizza.id} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGridStyles>
   );
 };
