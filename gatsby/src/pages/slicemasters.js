f3759bc8db6959e8bdceb942faee051900ad2e65
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-28 14:37:47 -0300

    Slicemaster page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,11 +1,82 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
-import { Layout } from '../components/Layout';
-import { Nav } from '../components/Nav';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
 
-const SliceMastersPage = () => (
-  <>
-    <p>He</p>
-  </>
-);
+const SlicemasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+const SliceMastersPage = ({ data }) => {
+  const slicemasters = data.slicemasters.nodes;
+  return (
+    <>
+      <SlicemasterGrid>
+        {slicemasters.map((person) => (
+          <SlicemasterStyles>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <span className='mark'>{person.name}</span>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className='description'>{person.description}</p>
+          </SlicemasterStyles>
+        ))}
+      </SlicemasterGrid>
+    </>
+  );
+};
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default SliceMastersPage;
