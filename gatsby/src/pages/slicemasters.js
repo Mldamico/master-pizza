73d3b0dfbd0a06a8d45b465605da00ccf3ea22ca
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-28 16:08:09 -0300

    Change query on slicemasters to limit

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -57,8 +57,8 @@ const SliceMastersPage = ({ data }) => {
 };
 
 export const query = graphql`
-  query {
-    slicemasters: allSanityPerson {
+  query($skip: Int = 0, $pageSize: Int = 2) {
+    slicemasters: allSanityPerson(limit: $pageSize, skip: $skip) {
       totalCount
       nodes {
         name
