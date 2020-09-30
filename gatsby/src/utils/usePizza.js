d9398792142d8ac606fad4095f9d892489359885
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-30 19:39:04 -0300

    Changing usePizza hhook for serverless

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,9 +1,15 @@
 import { useState, useContext } from 'react';
 import OrderContext from '../components/OrderContext';
+import { attachNamesAndPrices } from './attachNamesAndPrices';
+import { calculateOrderTotal } from './calculateOrderTotal';
+import formatMoney from './formatMoney';
 
-export const usePizza = ({ pizzas, input }) => {
+export const usePizza = ({ pizzas, values }) => {
   // const [order, setOrder] = useState([]);
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   const addToOrder = (orderedPizza) => {
     setOrder([...order, orderedPizza]);
@@ -13,5 +19,47 @@ export const usePizza = ({ pizzas, input }) => {
     setOrder([...order.slice(0, index), ...order.slice(index + 1)]);
   };
 
-  return { order, addToOrder, removeFromOrder };
+  const submitOrder = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeholder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+
+    const text = JSON.parse(await res.text());
+
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false);
+      setError(text.message);
+    } else {
+      setLoading(false);
+      setMessage('Success! Come on down for your pizza!');
+    }
+  };
+
+  return {
+    order,
+    addToOrder,
+    removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
+  };
 };
