6110000824ee1a8df03c318165332f91edb45840
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-02 10:51:58 -0300

    Loading grid

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Layout } from '../components/Layout';
 import { useLatestdata } from '../utils/useLatestdata';
+import { HomePageGrid } from '../styles/Grids';
+import { LoadingGrid } from '../components/LoadingGrid';
 
-const CurrentlySlicing = () => {
-  return <p>H</p>;
+const CurrentlySlicing = ({ slicemasters }) => {
+  return (
+    <div>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now</p>
+      )}
+    </div>
+  );
 };
-const HotSlices = () => {
-  return <p>h</p>;
+const HotSlices = ({ hotSlices }) => {
+  return (
+    <div>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothing in the Case</p>}
+    </div>
+  );
 };
 
 const HomePage = () => {
@@ -16,10 +29,10 @@ const HomePage = () => {
     <div className='center'>
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
-      <div>
+      <HomePageGrid>
         <CurrentlySlicing slicemasters={slicemasters}></CurrentlySlicing>
         <HotSlices hotSlices={hotSlices}></HotSlices>
-      </div>
+      </HomePageGrid>
     </div>
   );
 };
